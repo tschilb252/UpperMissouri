ec3152d9e4b86b9cb40a0ee23e309da502cafd34
Broman <dbroman@usbr.gov>
2018-04-26 07:37:39 -0600

    updated BigHole r script

---
+++ AdaptationStrategies/src/BigHole.r
@@ -0,0 +1,24 @@
+#################################################
+#' @title Upper Missouri Basin Study - Big Hole
+#'        Basin Figures
+#' @author Dan Broman
+#' @description Summary figures for the Upper Missouri
+# Basin Study, Big Hole River Basin
+#################################################
+library(tidyverse)
+library(data.table)
+
+#################################################
+#' User Inputs
+# Data Directories
+dirInp =
+dirOup =
+
+datBase =
+dat40cfs =
+dat20cfs =
+datNoMin =
+# LookUp Table Locations
+
+#################################################
+#'  Read in Data
