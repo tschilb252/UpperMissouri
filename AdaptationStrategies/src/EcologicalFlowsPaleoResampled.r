6493825e264af075589759335703bf2ad4ededaa
Broman <dbroman@usbr.gov>
2018-06-29 16:53:42 -0600

    updated ecoflow bar figure script

--- AdaptationStrategies/src/EcologicalFlowsPaleoResampled.r
+++ AdaptationStrategies/src/EcologicalFlowsPaleoResampled.r
@@ -32,7 +32,7 @@ fileList = unique(MeasTbl$File)
 stgyList = unique(StgyTbl$Strategy)
 ctFiles = nrow(StgyTbl)
 
-# CFRAnnFlushingRelease
+# cFRFlushingRelease
 fileTmp = fileList[1]
 datMeas = data.table()
 for(iterFile in 1:ctFiles){
@@ -65,11 +65,48 @@ datMeasAgg3 = datMeasAgg2 %>%
   group_by(Slot, Period, Scenario) %>%
   dplyr::summarise(ValueMin = min(Value), ValueMax = max(Value), ValueMedian = median(Value))
 
-datMeasAgg3$Scenario = factor(datMeasAgg3$Scenario,
-    levels = c('Historical', 'HD', 'HW', 'CT', 'WD', 'WW'))
+# TiberFlushingRelease
+fileTmp = fileList[2]
+datMeas2 = data.table()
+for(iterFile in 1:ctFiles){
+  filePath = paste0(dirInp, StgyTbl$Directory[iterFile], '/', fileTmp)
+  ScenarioSet = StgyTbl$ScenarioSet[iterFile]
+  Strategy =  StgyTbl$Strategy[iterFile]
+  datTmp = read.rdf(filePath)
+  datTmpDT = Rdf2dt(datTmp)
+  datTmpDT$ScenarioSet = ScenarioSet
+  datTmpDT$Strategy = Strategy
+  datMeas2 = bind_rows(datMeas2, datTmpDT)
+}
+
+datMeas2Agg = datMeas2 %>%
+  left_join(ScenTbl) %>%
+  filter(Scenario %in% ScenList) %>%
+  mutate(Scenario = ifelse(nchar(Scenario) == 5, substr(Scenario, 3,5), Scenario)) %>%
+    mutate(Wyear = wyear(Date), Month = month(Date)) %>%
+    mutate(ValueFlag = ifelse(Value > 0, 1, 0)) %>%
+    dplyr::rename(Slot = RiverWareSlot) %>%
+    group_by(Wyear, Trace, ScenarioSet, Strategy, Scenario, Slot, Period) %>%
+    dplyr::summarise(Value = sum(ValueFlag, na.rm = T))
+
+datMeas2Agg2 = datMeas2Agg %>%
+  mutate(ValueFlag = ifelse(Value > 0, 1, 0)) %>%
+  group_by(Trace, ScenarioSet, Strategy, Scenario, Slot, Period) %>%
+  dplyr::summarise(Value = sum(ValueFlag, na.rm = T))
+
+datMeas2Agg3 = datMeas2Agg2 %>%
+  group_by(Slot, Period, Scenario) %>%
+  dplyr::summarise(ValueMin = min(Value), ValueMax = max(Value), ValueMedian = median(Value))
+
+datMeasPlot = bind_rows(datMeasAgg3, datMeas2Agg3)
+
+datMeasPlot = datMeasPlot %>% left_join(MeasTbl)
+
+datMeasPlot$Scenario = factor(datMeasPlot$Scenario,
+    levels = rev(c('Historical', 'HD', 'HW', 'CT', 'WD', 'WW')))
 
 # Plots Canyon Ferry Years with Releases
-datMeasPlotFl = datMeasAgg3 %>% filter(Period %in% c('Historical', '2050s'))
+datMeasPlotFl = datMeasPlot %>% filter(Period %in% c('Historical', '2050s'))
 
 ggplot() +
   geom_linerange(data = datMeasPlotFl, aes(x = Scenario,
@@ -84,4 +121,6 @@ ggplot() +
   		strip.background = element_rect(fill = F),
   		strip.text = element_text(size = 15)) +
   	xlab('') +
-  	ylab('')
+  	ylab('') +
+    facet_wrap(~Measure) +
+    coord_flip()
